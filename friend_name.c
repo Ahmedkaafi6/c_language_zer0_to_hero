de9b307ede56659dcce7b4ec68544cdab68b6b0a
AHMED HASSAN <118650161+Ahmedkaafi6@users.noreply.github.com>
2022-12-11 14:27:36 +0300

    Update friend_name.c

--- friend_name.c
+++ friend_name.c
@@ -3,3 +3,21 @@
 // This is my bff. 
 
 // Good Luck
+#include <stdio.h>
+
+int main()
+{
+    char NAME[50] = "AHMED";
+    char name[50];
+    printf("Enter a Name: ");
+    scanf("%s", name);
+    if(strcmp(name, NAME)== 0){
+        printf("hurray this is me");
+    }
+        else{
+            printf("this is my best friend");
+        
+    }
+
+    return 0;
+}
